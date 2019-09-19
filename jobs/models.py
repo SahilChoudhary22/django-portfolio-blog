8fa6967e87ffb3172063836fa325013743563a3e
Sahil Choudhary <sahil.codes.it@gmail.com>
2019-09-19 17:03:23 +0530

    font edit, job model modified

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-	image = models.ImageField(upload_to = 'images/')
-	summary = models.CharField(max_length=200)
-	github = models.URLField(default = "https://github.com/", max_length=400)
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to = 'images/')
+    summary = models.CharField(max_length=200)
+    github = models.URLField(default = "https://github.com/", max_length=400)
+    
+    def __str__(self):
+        return self.title
